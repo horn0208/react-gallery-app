362c08af5bd2065f6a0f9761202d41afeb57310d
Liz Horn <liz.mpls@gmail.com>
2022-06-02 17:25:08 -0500

    like button works

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,13 +3,17 @@ import {useState} from 'react';
 function GalleryItem(props){
     //click to toggle content (hook)
     const [show, setShow] = useState(true);
+    const [like, setLike] = useState(0);
     //swap image with description on click using conditional rendering
     const toggleShow =()=>{
         setShow(!show);
     }
     //TODO--like button click: use axios to 'PUT' the like count /gallery/like/:id
     //TODO--update gallery each time like button is clicked (GET)
-
+    const countLike =()=>{
+        setLike(like+1);
+        console.log(like);
+    }
 
 
     return(
@@ -24,6 +28,7 @@ function GalleryItem(props){
                     <p>{props.photo.description}</p>
                 }
             </div>
+            <button onClick={countLike}>Like</button>
         </div>
     );
 }
