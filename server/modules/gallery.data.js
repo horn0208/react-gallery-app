f11058da48b83f79cdbc7c0e940ed62f58bf6fa0
Liz Horn <liz.mpls@gmail.com>
2022-06-02 11:07:35 -0500

    setup and added jpg files

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/vietnam.jpg', description: 'On a beach in Vietnam, a few days before Pi proposed.', likes: 0 },
+    { id: 2, path: 'images/laurel.jpg', description: 'Laurel on her first trip to the dog park: unbridled joy.', likes: 0 },
+    { id: 3, path: 'images/laos.jpg', description: 'Falls outside Luang Prabang, Laos.', likes: 0 },
+    { id: 4, path: 'images/font.jpg', description: 'Climbing in the forest of Fontainebleau, France.', likes: 0 },
+    { id: 5, path: 'images/moto.jpg', description: 'Motorbike rides with Pi on Cat Ba Island, Vietnam', likes: 0 },
+    { id: 6, path: 'images/palisade.jpg', description: 'Rachelle climbing at Palisade Head, before we got the first female ascents of Oz.', likes: 0 },
 ];
 
 module.exports = galleryItems;
