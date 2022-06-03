1b777ee1e367d43dbbe0a16b3a4ea97f95ab0efb
Liz Horn <liz.mpls@gmail.com>
2022-06-03 15:31:39 -0500

    database connected, GET works

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const pool = require('../modules/pool');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+//for BASE MODE:
+// const galleryItems = require('../modules/gallery.data');
+const galleryItems = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
@@ -16,9 +19,21 @@ router.put('/like/:id', (req, res) => {
     res.sendStatus(200);
 }); // END PUT Route
 
-// GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+// GET Route BASE MODE:
+// router.get('/', (req, res) => {
+//     res.send(galleryItems);
+// }); // END GET Route
+
+// GET Route to database:
+router.get('/', (req, res)=>{
+    console.log('in router GET');
+    const queryString = `SELECT * FROM gallery;`;
+    pool.query(queryString).then((result)=>{
+        res.send(result.rows);
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(500);
+    })
+})
 
 module.exports = router;
